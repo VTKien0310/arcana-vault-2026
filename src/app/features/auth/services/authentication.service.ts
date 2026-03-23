bf00f7cd354f276bad21f1706ee86b34fee22bea
trungkien <trgkien.vu@gmail.com>
2026-03-23 12:00:16 +0700

    ref: Simplify authentication service and remove state management
    - Removed `AuthenticationState` and related logic from `authentication.state.ts`.
    - Updated `AuthenticationService` to directly handle user state with methods like `getUser`, `getSession`, and `registerOnAuthStateChange`.
    - Improved `AuthenticationService` by eliminating dependency on `AuthenticationState` for cleaner, self-contained logic.

--- src/app/features/auth/services/authentication.service.ts
+++ src/app/features/auth/services/authentication.service.ts
@@ -1,13 +1,12 @@
 import {inject, Injectable} from '@angular/core';
 import {SupabasePort} from '@ports/supabase/supabase.port';
 import {ToastPort} from '@ports/toast/toast.port';
-import {AuthenticationState} from '@features/auth/states/authentication.state';
+import {AuthChangeEvent, Session, User} from '@supabase/supabase-js';
 
 @Injectable()
 export class AuthenticationService {
   private supabase = inject(SupabasePort);
   private toast = inject(ToastPort);
-  private authState = inject(AuthenticationState);
 
   async login(email: string, password: string): Promise<boolean> {
     const {data, error} = await this.supabase.client.auth.signInWithPassword({
@@ -20,13 +19,33 @@ export class AuthenticationService {
         error?.message || 'An error occurred during login.',
       );
 
-      this.authState.setUser(null);
-
       return false;
     }
 
-    this.authState.setUser(data.user);
-
     return true;
+  }
+
+  async getUser(): Promise<User | null> {
+    const {data: {user}} = await this.supabase.client.auth.getUser();
+
+    return user;
+  }
+
+  async getSession(): Promise<Session | null> {
+    const {data: {session}} = await this.supabase.client.auth.getSession();
+
+    if (!session) {
+      await this.toast.error('Session not found.');
+    }
+
+    return session;
+  }
+
+  async registerOnAuthStateChange(): Promise<void> {
+    this.supabase.client.auth.onAuthStateChange(
+      (event: AuthChangeEvent, session: Session | null) => {
+
+      },
+    );
   }
 }
